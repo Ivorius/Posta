28d7103187ccd4176549694f459fff091774838d
Ivo Toman <ivorius@gmail.com>
2015-12-06 21:21:50 +0100

    resolve problem with new jquery

--- assets/js/anyTitle.js
+++ assets/js/anyTitle.js
@@ -1,34 +1,25 @@
 var anyPopisekClass = "anyTitle";
 var anyPopisekIdPlovouciDiv = "plovouciHlaska";
 
-$(document).ready(function()
-{
-        
-    $("."+anyPopisekClass).live("mouseover",function () 
-    { 
-        var titulek = $(this).attr('popis');
-        var animace = $(this).attr('animace');
-        
-        $("#"+anyPopisekIdPlovouciDiv).html(titulek);
-        if(animace == true) $("#"+anyPopisekIdPlovouciDiv).show(100);
-        else                $("#"+anyPopisekIdPlovouciDiv).show();
-    });
-    
-    $("."+anyPopisekClass).live("mouseout", function () 
-    { 
-        var animace = $(this).attr('animace');
-        if(animace == true) $("#"+anyPopisekIdPlovouciDiv).hide(100);
-        else                $("#"+anyPopisekIdPlovouciDiv).hide();
-    });
-    
-    $("."+anyPopisekClass).live("mousemove", function (e) 
-    { 
-        var mysX = e.pageX;
-        var mysY = e.pageY;
-        var obj = document.getElementById(anyPopisekIdPlovouciDiv);
-        
-        obj.style.top = mysY + "px";
-        obj.style.left = mysX + 12 + "px";
-    });
-    
-});
+$(document).on({
+    mouseenter: function () {
+        var title = $(this).attr('popis');
+        $('<div class="posta_tooltip"></div>')
+            .html(title)
+            .appendTo('body')
+            .fadeIn();
+    },
+
+    mouseleave: function() {
+        // Hover out code
+        //   $(this).attr('title', $(this).data('tipText'));
+        $('.posta_tooltip').remove();
+    },
+
+    mousemove: function(e) {
+        var mousex = e.pageX + 20; //Get X coordinates
+        var mousey = e.pageY + 10; //Get Y coordinates
+        $('.posta_tooltip')
+            .css({ top: mousey, left: mousex });
+    }
+}, "."+anyPopisekClass);
